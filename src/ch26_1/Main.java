39990d8b3e9f5f72ee153d0fcf3dd85e4c7b2586
WhiteCat <whitecatx6@gmail.com>
2019-01-02 22:14:17 +0800

    ch26 1

---
+++ src/ch26_1/Main.java
@@ -0,0 +1,12 @@
+package ch26_1;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame frame = new Frame("你的名字");
+        frame.setBackground(Color.pink);
+        frame.setSize(600, 300);
+        frame.setVisible(true);
+    }
+}
