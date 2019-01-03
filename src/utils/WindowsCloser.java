0400956b4458a24ea2e8dd729577e917f7b89080
WhiteCat <whitecatx6@gmail.com>
2019-01-03 12:58:35 +0800

    Add WindowsCloser to handle X

---
+++ src/utils/WindowsCloser.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class WindowsCloser extends WindowAdapter {
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+}
